6ba95276c6bfbd70d3dce265c1cfb83b53cb3a5a
Maik Allgöwer <maik@llgoewer.de>
2019-11-30 18:42:25 +0100

    Clean up web code

--- web/remote.js
+++ web/remote.js
@@ -1,19 +1,28 @@
-window.onload = function() {
-    let buttonConnect = document.getElementById("connect");
-    let ble = navigator.bluetooth;
-    charVendor = null;
-    charFunction = null;
+function BleRemote(buttonConnect) {
+    this.ble = {
+        server: null,
+        vendor: null,
+        func: null,
+    };
 
-    buttonConnect.addEventListener('pointerup', function(evt) {
+    this.buttonConnect = buttonConnect;
+
+    this.connect = function() {
+        let ble = navigator.bluetooth;
+
         ble.requestDevice({filters: [{namePrefix: "Puck.js"}]})
         .then(device => {
-            device.addEventListener("gattserverdisconnected", onDisconnect);
+            device.addEventListener("gattserverdisconnected", this.onDisconnect);
             console.log("Connecting to " + device.name);
+
             return device.gatt.connect();
         })
         .then(server => {
+            this.ble.server = server;
+
             buttonConnect.innerHTML = "Disconnect";
             console.log("Connection established");
+
             return server.getPrimaryService("c0050001-10b6-4fbb-bc5b-8b477f92267a");
         })
         .then(service => {
@@ -23,44 +32,67 @@ window.onload = function() {
             characteristics.forEach(c => {
                 switch (c.uuid.slice(4, 8)) {
                     case '0002':
-                        charVendor = c;
+                        this.ble.vendor = c;
                         break;
                     case '0003':
-                        charFunction = c;
+                        this.ble.func = c;
                         break;
                 };
             });
         })
-        .catch(error => {console.log(error);});
-    });
-};
+        .catch(error => {
+            this.ble.server = null;
+            this.ble.vendor = null;
+            this.ble.func =null;
 
-function sendCode(code) {
-    let vendor = new Uint8Array(1);
-    let func = new Uint8Array(1);
+            console.log(error);
+        });
+    };
 
-    vendor[0] = 0;
-    func[0] = code;
-
-    if (charVendor && charFunction) {
-        charVendor.writeValue(vendor)
-        .then(_ => {
-            return charFunction.writeValue(func);
-        })
-        .then(_ => {
-            console.log("Sent: vendor =  " + vendor[0] + ", function = " + func[0]);
-        })
-        .catch(error => { console.log(error); });
+    this.connected = function() {
+        if (this.ble.server)
+            return this.ble.server.connected;
+        else
+            return false;
     }
-}
 
-function onDisconnect(event) {
-    let device = event.target;
-    console.log("Device " + device.name + " disconnected");
+    this.onDisconnect = function(event) {
+        let device = event.target;
+        console.log("Device " + device.name + " disconnected");
+    }
 
-    charVendor = null;
-    charFunction = null;
-    document.getElementById("connect").innerHTML = "Connect";
-}
+    this.disconnect = function() {
+        this.ble.server.disconnect();
 
-console.log("Setup complete");
+        this.ble.server = null;
+        this.ble.vendor = null;
+        this.ble.func = null;
+
+        this.buttonConnect.innerHTML = "Connect";
+    };
+
+    this.send = function(code) {
+        let vendor = new Uint8Array(1);
+        let func = new Uint8Array(1);
+
+        vendor[0] = 0;
+        func[0] = code;
+
+        if (this.ble.vendor && this.ble.func) {
+            this.ble.vendor.writeValue(vendor)
+            .then(_ => {
+                return this.ble.func.writeValue(func);
+            })
+            .then(_ => {
+                console.log("Sent: vendor =  " + vendor[0] + ", function = " + func[0]);
+            })
+            .catch(error => {
+                this.ble.server = null;
+                this.ble.vendor = null;
+                this.ble.func =null;
+
+                console.log(error);
+            });
+        }
+    };
+};
